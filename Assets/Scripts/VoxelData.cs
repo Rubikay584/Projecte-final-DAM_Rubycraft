fc287671e2c9b97831e259ac6ece7edd6559fe4b
Rubyrak <rubyrak.games@gmail.com>
2025-03-26 17:40:51 +0100

    Part 16 - Lighting 2 (Arreglades algunes coses)

--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -9,8 +9,8 @@ public static class VoxelData {
     public static readonly int WorldSizeInChunks = 10;
 
     // Lightning Values
-    public static float minLightLevel = 0.15f;
-    public static float maxLightLevel = 0.8f;
+    public static float minLightLevel = 0.1f;
+    public static float maxLightLevel = 0.9f;
     public static float lightFalloff = 0.08f;
 
     public static int WorldSizeInVoxels {
